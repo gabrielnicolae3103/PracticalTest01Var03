f74d1c412c27c3b63846774879f1230a08217f74
Gabriel Nicolae <gabriel.nicolae313@gmail.com>
2021-04-12 12:58:03 +0300

    B

---
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/Constants.java
@@ -0,0 +1,7 @@
+package ro.pub.cs.systems.eim.practicaltest01var03;
+
+public class Constants {
+    final public static String NUMBER_1 = "N1";
+    final public static String NUMBER_2 = "N2";
+    final public static String RESULT = "R";
+}
--- PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -2,13 +2,116 @@ package ro.pub.cs.systems.eim.practicaltest01var03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    EditText number1;
+    EditText number2;
+    Button plusButton;
+    Button minusButton;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var03_main);
+
+        this.number1 = findViewById(R.id.number1EditText);
+        this.number2 = findViewById(R.id.number2EditText);
+        this.plusButton = findViewById(R.id.plusButton);
+        this.minusButton = findViewById(R.id.minusButton);
+        this.result = findViewById(R.id.result);
+
+        this.plusButton.setOnClickListener(new ViewListener());
+        this.minusButton.setOnClickListener(new ViewListener());
+    }
+
+
+    public class ViewListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.plusButton: {
+                    String n1 = number1.getText().toString();
+                    String n2 = number2.getText().toString();
+                    if (!isInteger(n1) || !isInteger(n2)) {
+                        Toast.makeText(getApplicationContext(), "Introdu numere", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    int resultN = Integer.parseInt(n1) + Integer.parseInt(n2);
+                    String resultText = n1 + " + " + n2 + " = " + String.valueOf(resultN);
+                    result.setText(resultText);
+                    break;
+                }
+                case R.id.minusButton: {
+                    String n1 = number1.getText().toString();
+                    String n2 = number2.getText().toString();
+                    if (!isInteger(n1) || !isInteger(n2)) {
+                        Toast.makeText(getApplicationContext(), "Introdu numere", Toast.LENGTH_LONG).show();
+                        break;
+                    }
+                    int resultN = Integer.parseInt(n1) - Integer.parseInt(n2);
+                    String resultText = n1 + " - " + n2 + " = " + String.valueOf(resultN);
+                    result.setText(resultText);
+                    break;
+                }
+                default: {
+                    return;
+                }
+
+            }
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        return isInteger(s,10);
+    }
+
+    public static boolean isInteger(String s, int radix) {
+        if(s.isEmpty() || s == null) return false;
+        for(int i = 0; i < s.length(); i++) {
+            if(i == 0 && s.charAt(i) == '-') {
+                if(s.length() == 1) return false;
+                else continue;
+            }
+            if(Character.digit(s.charAt(i),radix) < 0) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(Constants.NUMBER_1, number1.getText().toString());
+        savedInstanceState.putString(Constants.NUMBER_2, number2.getText().toString());
+        savedInstanceState.putString(Constants.RESULT, result.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String s1 = "";
+        String s2 ="";
+        if (savedInstanceState.containsKey(Constants.NUMBER_1)) {
+            s1 = savedInstanceState.getString(Constants.NUMBER_1);
+            number1.setText(s1);
+        }
+        if (savedInstanceState.containsKey(Constants.NUMBER_2)) {
+            s2 = savedInstanceState.getString(Constants.NUMBER_2);
+            number2.setText(s2);
+        }
+        if (savedInstanceState.containsKey(Constants.RESULT)) {
+            String s3 = savedInstanceState.getString(Constants.RESULT);
+            result.setText(s3);
+        }
+        Toast.makeText(getApplicationContext(), "Numerele sunt " + s1 + " " + s2, Toast.LENGTH_LONG).show();
     }
 }
